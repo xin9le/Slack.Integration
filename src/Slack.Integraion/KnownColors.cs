52f084f4ab9879125217ef6ae31e3db738cf5d33
Takaaki Suzuki <xin9le@live.jp>
2018-11-27 19:54:48 +0900

    Add slack known color definitions.

---
+++ src/Slack.Integraion/KnownColors.cs
@@ -0,0 +1,41 @@
+﻿namespace Slack.Integraion
+{
+    /// <summary>
+    /// Provides known color pattern on Slack.
+    /// Like traffic signals.
+    /// </summary>
+    public static class KnownColors
+    {
+        /// <summary>
+        /// Gets a good color. This value is constant.
+        /// 
+        /// <para>
+        /// RGB : (49, 163, 82)
+        /// HEX : #31A352
+        /// </para>
+        /// </summary>
+        public const string Good = "good";
+
+
+        /// <summary>
+        /// Gets a warning color. This value is constant.
+        /// 
+        /// <para>
+        /// RGB : (221, 159, 62)
+        /// HEX : #DD9F3E
+        /// </para>
+        /// </summary>
+        public const string Warning = "warning";
+
+
+        /// <summary>
+        /// Gets a danger color. This value is constant.
+        /// 
+        /// <para>
+        /// RGB : (212, 24, 24)
+        /// HEX : #D41818
+        /// </para>
+        /// </summary>
+        public const string Danger = "danger";
+    }
+}
