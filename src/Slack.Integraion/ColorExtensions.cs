1d529061f48398309dd7f7c94a1588ca01d8d9ba
Takaaki Suzuki <xin9le@live.jp>
2018-11-27 19:58:34 +0900

    Add color extensions.

---
+++ src/Slack.Integraion/ColorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+
+
+namespace Slack.Integraion
+{
+    /// <summary>
+    /// <see cref="Color"/> の拡張機能を提供します。
+    /// </summary>
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// Converts to 16 hex string. (eg. #F0F8FF)
+        /// </summary>
+        /// <param name="color">value</param>
+        /// <returns></returns>
+        public static string ToHex(this Color color)
+            => $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+    }
+}
