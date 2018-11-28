6b2722c35bab5a31c70df7a8465643798531cd91
Takaaki Suzuki <xin9le@live.jp>
2018-11-28 17:48:06 +0900

    Add DateTime to UnixTime seconds conversion.

---
+++ src/Slack.Integration/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace Slack.Integration
+{
+    /// <summary>
+    /// Provides <see cref="DateTime"/> extension functions.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        #region Constants
+        /// <summary>
+        /// Gets the Unix Epoch. This valus is constant.
+        /// </summary>
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Gets elapsed seconds from Unix Epoch (1970/01/01 00:00:00).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static long ToUnixTimeSeconds(this DateTime value)
+        {
+            var utc = value.ToUniversalTime();
+            var elapsed = utc - UnixEpoch;
+            return (long)elapsed.TotalSeconds;
+        }
+        #endregion
+    }
+}
